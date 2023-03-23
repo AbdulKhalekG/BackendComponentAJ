cf49f59d5e947f6c4af882c24b5ed5fb14062140
Jesus Perez <66017490+JesusP8@users.noreply.github.com>
2023-03-22 23:21:43 -0400

    Cambios backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Serve the data.json file
+app.get('/data', (req, res) => {
+  res.sendFile(__dirname + '/data/data.json');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
