96dd770b100964811e4e956b7826dbec84d37f6d
AbdulKhalekG <adnan_akg_1401@hotmail.com>
2023-04-13 20:57:29 -0400

    LETS GO OTRO COMMIT

---
+++ back/src/database.js
@@ -0,0 +1,65 @@
+const { Pool } = require('pg')
+const  helpers  = require('./helpers')
+
+const config={
+    user:'postgres',
+    host:'localhost',
+    password:'123',
+    database:'sql'
+};
+//funciones
+const pool = new Pool(config);
+
+
+
+
+
+
+//funcion create user
+const createuser= async (req,res) =>{
+
+    const{ nombre, username, correo,clave} = req.body;
+    const passwordencriptado = await helpers.encryptPassword(clave)
+    const response = await pool.query('INSERT INTO usuarios (nombre, username, correo,clave) VALUES($1, $2, $3, $4)', [
+        nombre, username, correo,passwordencriptado])
+    console.log(response);
+    res.json(response.rows)
+}
+
+//funcion modify user
+const modifyuser=async (req,res)=>{
+
+    const{ nombre, username, correo ,clave,id_usuario} = req.body;
+    const response = await pool.query('UPDATE usuarios SET nombre= $1 username= $2, correo=$3, clave=$4 WHERE id_usuario=$5', [nombre, username, correo,clave,id_usuario])
+    console.log(response);
+    res.json(response.rows)
+
+}
+
+//funcion searchusername
+const searchusername=async(req,res)=>{
+    const username = req.params.username
+    const response= await pool.query('SELECT * FROM usuarios WHERE username=$1 ', [username])
+    console.log(response.rows);
+    res.json(response.rows)
+}
+
+
+//funcion searchuserid
+const searchuserid=async(req,res)=>{
+    const id_usuario =req.params.id_usuario
+    const response=await pool.query('SELECT * FROM usuarios WHERE id_usuario=$1', [id_usuario])
+    console.log(response.rows);
+    res.json(response.rows)
+}
+
+
+module.exports = {
+    createuser,
+    modifyuser,
+    searchuserid,
+    searchusername,
+
+
+
+}
